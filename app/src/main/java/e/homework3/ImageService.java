a60fbd0a7aff1b63b30a5bdd255879105d82cb52
glotovmikhail <glotov.mikhail@mail.ru>
2016-11-28 20:58:43 +0300

    done

---
+++ app/src/main/java/e/homework3/ImageService.java
@@ -0,0 +1,65 @@
+package e.homework3;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+
+/**
+ * Created by Михаил on 27.11.2016.
+ */
+
+public class ImageService extends Service implements Runnable {
+
+    private File file;
+    private static final String imageURL = "http://images-cdn.9gag.com/photo/a2YVb8e_700b.jpg";
+    public static byte[] buffer;
+    public static int temp;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        file = new File(getFilesDir(), MainActivity.IMAGE);
+        new Thread(this).start();
+        return START_STICKY;
+    }
+
+    @Override
+    public void run() {
+        InputStream in = null;
+        FileOutputStream out = null;
+        try {
+            URL URL = new URL(imageURL);
+            in = new BufferedInputStream(URL.openStream());
+            out = new FileOutputStream(file);
+            buffer = new byte[1024];
+            while ((temp = in.read(buffer)) != -1) {
+                out.write(buffer, 0, temp);
+            }
+            sendBroadcast(new Intent(MainActivity.BROADCAST));
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (in != null && out != null) {
+                    in.close();
+                    out.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
--- app/src/main/java/e/homework3/MainActivity.java
+++ app/src/main/java/e/homework3/MainActivity.java
@@ -1,13 +1,102 @@
 package e.homework3;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private File file;
+    private Bitmap bitmap;
+    private ImageView imageView;
+    private TextView textView;
+    private Button button;
+    private Button imitation;
+    private BroadcastReceiver imageReceiver;
+    private BroadcastReceiver broadcastReceiver;
+    public static final String IMAGE = "savedImage.jpg";
+    public static final String BROADCAST = "BROADCAST";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = (ImageView) findViewById(R.id.image);
+        textView = (TextView) findViewById(R.id.error_text);
+        file = new File(getFilesDir(), IMAGE);
+        bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
+
+        button = (Button) findViewById(R.id.button);
+        imitation = (Button) findViewById(R.id.imitation);
+
+        button.setOnClickListener(this);
+        imitation.setOnClickListener(this);
+
+        if (file.exists()) {
+            setImage();
+        } else setText();
+
+        broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                Intent in = new Intent(context, ImageService.class);
+                context.startService(in);
+            }
+        };
+        imageReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                file = new File(getFilesDir(), IMAGE);
+                bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
+
+                if (file.exists()) setImage();
+            }
+        };
+        registerReceiver(broadcastReceiver, new IntentFilter(Intent.ACTION_POWER_CONNECTED));
+        registerReceiver(imageReceiver, new IntentFilter(BROADCAST));
+    }
+
+    private void setImage() {
+        imageView.setImageBitmap(bitmap);
+        textView.setVisibility(View.INVISIBLE);
+        imageView.setVisibility(View.VISIBLE);
+        button.setVisibility(View.VISIBLE);
+        imitation.setVisibility(View.INVISIBLE);
+    }
+
+    private void setText() {
+        textView.setVisibility(View.VISIBLE);
+        imageView.setVisibility(View.INVISIBLE);
+        button.setVisibility(View.INVISIBLE);
+        imitation.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(imageReceiver);
+        unregisterReceiver(broadcastReceiver);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == button) {
+            file.delete();
+            setText();
+        } else {
+            broadcastReceiver.onReceive(this, new Intent(Intent.ACTION_POWER_CONNECTED));
+        }
+
     }
 }
